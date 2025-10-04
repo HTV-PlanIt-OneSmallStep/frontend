1e23b9bb113758152c6a8628fd36ec366d7f49ab
Clare Zhu <clare.zhu@mail.utoronto.ca>
2025-10-04 18:54:37 -0400

    Added form components

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -4,7 +4,7 @@ import './form.css';
 
 
 export default function Form({ closeForm, showForm }) {
-    const[deadline, setDeadline] = React.useState(false);
+    const[deadline, setDeadline] = React.useState(true);
 
     return (
         <form className={showForm ? "add-task-form" : "add-task-form hidden"}>
@@ -17,10 +17,19 @@ export default function Form({ closeForm, showForm }) {
             <input type="checkbox" />
 
             <label htmlFor="start"><b>{deadline ? "Start Date" : "Date"}</b></label>
-            <input type="date" name="begin" value="" min="2025-10-04"/>
+            <input type="date" name="beginDate" value="" min="2025-10-04"/>
+            <input type="time" name="beginTime" />
 
-            <label htmlFor="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required />
+            <div>
+                <label htmlFor='end' >End Date</label>
+                <input type="date" name="endDate" value="" min="2025-10-04"/>
+                <input type="time" name="endTime" />
+            </div>
+
+
+            <label htmlFor="psw"><b>Context</b></label>
+            <p>What details about this event/project can you give?</p>
+            <textarea id="context" name="context" rows="4" cols="50" />
 
             <NavLink to="/calendar" className={({isActive}) => isActive ? 'active' : ''}>
                 Generate Schedule

37dcb5b97b4903941d41a08b82daba72eb3b25ac
Penguinfami <christine.spyphone.zhao@gmail.com>
2025-10-04 20:06:00 -0400

    Add connection to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Outlet, NavLink } from 'react-router-dom'
+import dotenv from 'dotenv'
 import './App.css'
 
+dotenv.config();
+
 function App() {
+  
   return (
     <div className="app-root">
       <main style={{padding: 16}}>
--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,15 @@ import axios from "axios"
 
 export const apiClient = axios.create(
     {
-        baseURL: 'https://some-domain.com/api/',
+        baseURL: process.env.REACT_APP_BACKEND_URL,
+        timeout: 1000
+    }
+)
+
+
+export const apiTaskClient = axios.create(
+    {
+        baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/plan/schedule`,
         timeout: 1000
     }
 )

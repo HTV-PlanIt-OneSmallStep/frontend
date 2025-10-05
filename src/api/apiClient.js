b6cc568b09ad7e3c6e55fa07f7bd14a0b7b17bf9
Penguinfami <christine.spyphone.zhao@gmail.com>
2025-10-05 00:20:45 -0400

    increase timeout of api call

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const apiClient = axios.create(
     {
         baseURL: 'http://localhost:3000',//process.env.REACT_APP_BACKEND_URL,
-        timeout: 1000
+        timeout: 4000
     }
 )
 
@@ -11,6 +11,6 @@ export const apiClient = axios.create(
 export const apiTaskClient = axios.create(
     {
         baseURL: `http://localhost:3000/api/plan/schedule`,
-        timeout: 1000
+        timeout: 4000
     }
 )
